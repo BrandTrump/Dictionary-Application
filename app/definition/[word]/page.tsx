6c02bad8ec2ff8f12593805fd060d40a9a183e4a
Brandon Trump <brandontrump101@gmail.com>
2023-05-07 00:46:22 +1200

    feat: included audio file of phonetic

--- app/definition/[word]/page.tsx
+++ app/definition/[word]/page.tsx
@@ -12,34 +12,75 @@ async function DefinitionPage({ params: { word } }: Props) {
 
   return (
     <div className="space-y-5">
-      <div>
-        <div>
+      <div className="space-y-2 md:flex md:justify-between md:items-center">
+        <div className="space-y-2">
           <h1 className="text-5xl capitalize">{data.word}</h1>
           <h2 className="text-fuchsia-600 font-bold">{data.phonetic}</h2>
         </div>
+        {data.phonetics.map((phontic, index) => (
+          <div key={index} className="flex justify-center ">
+            {phontic.audio ? (
+              <audio controls src={phontic.audio}>
+                This browser does not support this file
+              </audio>
+            ) : null}
+          </div>
+        ))}
       </div>
       {data.meanings.map((meaning, index) => (
         <div key={index}>
-          <p className="italic font-bold mb-5">{meaning.partOfSpeech}</p>
-          <p className="text-slate-500">Meaning</p>
+          <div className="flex items-center space-x-5 mb-5">
+            <p className="italic font-bold flex-shrink">
+              {meaning.partOfSpeech}
+            </p>
+            <hr className="h-px bg-gray-200 border-0 flex-grow"></hr>
+          </div>
+          <p className="text-slate-500 mb-4">Meaning</p>
           <ul
             role="list"
             className="marker:text-fuchsia-600 list-disc pl-5 space-y-3"
           >
             {meaning.definitions.map((definition, index) => (
-              <li key={index}>{definition.definition}</li>
+              <li key={index}>
+                {definition.definition}
+                <p className="text-slate-500">
+                  {definition?.example ? `"${definition!.example}"` : null}
+                </p>
+              </li>
             ))}
           </ul>
-          <p className="text-slate-500">
-            Synonyms{" "}
+          <p className="text-slate-500 mt-6">
+            {meaning.synonyms.length === 0 ? null : "Synonyms"}{" "}
             <span className="text-fuchsia-600 font-bold">
               {meaning.synonyms}
             </span>
           </p>
         </div>
       ))}
-      <p className="text-slate-500">Source</p>
-      <Link href={data.sourceUrls[0]}>{data.sourceUrls}</Link>
+      <hr className="h-px bg-gray-200 border-0"></hr>
+
+      <div className="md:inline-flex space-x-4">
+        <p className="text-slate-500">Source</p>
+        <div className="flex items-center space-x-2">
+          <Link href={data.sourceUrls[0]} className="underline">
+            {data.sourceUrls}
+          </Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-4 h-4 text-slate-500"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+            />
+          </svg>
+        </div>
+      </div>
     </div>
   );
 }
