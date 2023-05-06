733b9fbdfeb1c7468906e688c9630db10475b170
Brandon Trump <brandontrump101@gmail.com>
2023-05-07 01:55:06 +1200

    style: centered text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl">Home Page</h1>
+      <h1 className="text-3xl text-center mt-10">
+        Frontend Mentor Challenge | Dictionary Word Application
+      </h1>
     </div>
   );
 }
