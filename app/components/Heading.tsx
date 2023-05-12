965f609d96bae5a1ce8fd569557b035eaf8827f9
Brandon Trump <brandontrump101@gmail.com>
2023-05-12 17:08:59 +1200

    feat: changed the book icon to a home link

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 function Heading() {
   return (
-    <div className="flex justify-between items-center mb-4">
-      <div>
+    <div className="flex justify-between items-center mb-8">
+      <Link href={"/"}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           strokeWidth="1.5"
           stroke="currentColor"
-          className="w-12 h-12 text-slate-500"
+          className="w-12 h-12 text-gray-500"
         >
           <path
             strokeLinecap="round"
@@ -16,7 +18,7 @@ function Heading() {
             d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
           />
         </svg>
-      </div>
+      </Link>
 
       <div className="flex divide-x-2 items-center">
         <div className="pr-4 font-bold text-sm">
