97e5f9f12c77b2930ed93d504fb886845e8c3fce
Brandon Trump <brandontrump101@gmail.com>
2023-05-14 21:47:23 +1200

    feat: added link to github repo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col justify-center space-y-10 text-center">
       <h1 className="text-3xl text-center mt-10">
         Frontend Mentor Challenge | Dictionary Word Application
       </h1>
+      <Link
+        href={"https://github.com/BrandTrump/Dictionary-Application"}
+        className="text-xl underline"
+      >
+        GitHub Repo
+      </Link>
     </div>
   );
 }
