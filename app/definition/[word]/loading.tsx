bbf80e9c1a63311497cb16af2279784188c922ea
Brandon Trump <brandontrump101@gmail.com>
2023-05-14 21:45:50 +1200

    style: added text color for dark mode

--- app/definition/[word]/loading.tsx
+++ app/definition/[word]/loading.tsx
@@ -1,7 +1,7 @@
 function loading() {
   return (
     <div className="mt-20">
-      <h1 className="font-bold text-center animate-pulse text-gray-600 uppercase">
+      <h1 className="font-bold text-center animate-pulse text-gray-600  dark:text-white uppercase">
         Loading
       </h1>
     </div>
