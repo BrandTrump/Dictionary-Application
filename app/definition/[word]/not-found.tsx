40614ca215bae9fcfc91aa70111948fa23938c92
Brandon Trump <brandontrump101@gmail.com>
2023-05-16 21:50:11 +1200

    feat & style: created a custom Not Found UI

---
+++ app/definition/[word]/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center space-y-10 mt-32 text-center">
+      <h2 className="font-bold text-4xl uppercase">
+        Sorry I Could&apos;t Find a Definition
+      </h2>
+      <p className="text-2xl">Please try search for another word or phrase.</p>
+    </div>
+  );
+}
