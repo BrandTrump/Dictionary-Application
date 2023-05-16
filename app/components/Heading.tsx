f57bc1f0d5a1fe0c0d079feee30397a8b1cbe7e4
Brandon Trump <brandontrump101@gmail.com>
2023-05-16 21:46:44 +1200

    fix: changed svg tags

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -28,13 +28,13 @@ function Heading() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="w-6 h-6 text-fuchsia-600"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M19.5 8.25l-7.5 7.5-7.5-7.5"
             />
           </svg>
