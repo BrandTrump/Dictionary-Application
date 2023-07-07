8c44da9a3ee13e2c708fea697e6ec20062703a63
Brandon Trump <brandontrump101@gmail.com>
2023-07-07 16:06:52 +1200

    feat: updated loading page

--- app/definition/[word]/loading.tsx
+++ app/definition/[word]/loading.tsx
@@ -1,9 +1,20 @@
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 function loading() {
   return (
-    <div className="mt-20">
-      <h1 className="font-bold text-center animate-pulse text-gray-600  dark:text-white uppercase">
-        Loading
-      </h1>
+    <div className="mt-20 flex flex-col space-y-6">
+      <Skeleton height={20} width={250} />
+      <hr />
+      <div className="flex flex-col space-y-5">
+        <Skeleton count={5} />
+      </div>
+      <hr />
+      <div className="flex flex-col space-y-10">
+        <Skeleton count={4} />
+      </div>
+      <hr />
+      <Skeleton width={200} />
     </div>
   );
 }
