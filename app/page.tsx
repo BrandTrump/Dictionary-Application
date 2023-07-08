b436d1c673dbc0cce8bdf9dfbdd127874455cc9c
Brandon Trump <brandontrump101@gmail.com>
2023-07-08 14:40:59 +1200

    style: centered content

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center space-y-10 text-center">
+    <div className="flex flex-col justify-center items-center space-y-10 text-center mt-[45%] md:mt-[40%]">
       <h1 className="text-3xl text-center mt-10">
         Frontend Mentor Challenge | Dictionary Word Application
       </h1>
