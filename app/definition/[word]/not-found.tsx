29f8ef689c35395a0109027a3336409966254a75
Brandon Trump <brandontrump101@gmail.com>
2023-07-08 14:42:53 +1200

    style: centered content

--- app/definition/[word]/not-found.tsx
+++ app/definition/[word]/not-found.tsx
@@ -1,10 +1,10 @@
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center space-y-10 mt-32 text-center">
-      <h2 className="font-bold text-4xl uppercase">
+    <div className="flex flex-col items-center space-y-10 text-center mt-[50%] md:mt-[40%]">
+      <h2 className="font-bold text-2xl uppercase">
         Sorry I Could&apos;t Find a Definition
       </h2>
-      <p className="text-2xl">Please try search for another word or phrase.</p>
+      <p className="text-xl">Please try search for another word or phrase.</p>
     </div>
   );
 }
