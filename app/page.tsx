4966522b368bb8b08cabfd0859d1699edbaa3c8c
Brandon Trump <brandontrump101@gmail.com>
2023-07-13 18:18:54 +1200

    style: added a border

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div
-      className={`flex flex-col justify-center items-center space-y-10 text-center mt-[40%] md:mt-[40%]`}
+      className={`flex flex-col justify-center items-center space-y-10 text-center mt-[40%] md:mt-[40%] border-2 md:border-4 border-gray-400/50 border-dashed py-10`}
     >
       <h1 className="text-3xl text-center mt-10">
         Frontend Mentor Challenge | Dictionary Word Application
